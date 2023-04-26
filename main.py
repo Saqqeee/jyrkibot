84e05f5c25098142c381ff389d2fe85f104e2736
Saqqeee <131715608+Saqqeee@users.noreply.github.com>
2023-04-27 00:24:04 +0300

    Added huomenta.py
    Stuff for tracking sleep schedules

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import discord
+import sqlite3
 import json
 import os
 import logging
 import pytz
+from slashcommands import huomenta
 from datetime import datetime
 handler = logging.FileHandler(filename='loki.log', encoding='utf-8', mode='w')
 
@@ -22,10 +24,13 @@ else:
     ##    to one server; leave None to sync them globally
     data = {
         "TOKEN": input("Your Discord app's auth token: "),
-        "guild": None
+        "guild": None,
+        "owner": input("Your own Discord ID: "),
+        "rattimes": [4, 11]
     }
     with open("cfg/cfg.json", "w+") as confile:
         json.dump(data, confile)
+
 ## Read configurations from file and save them into variables
 with open("cfg/cfg.json", "r") as confile:
     config = json.load(confile)
@@ -34,6 +39,17 @@ if config["guild"] == None:
     gld = None
 else:
     gld = discord.Object(id=config["guild"])
+owner = config["owner"]
+rattimes = config["rattimes"]
+
+## Connect into database or create one if it doesn't already exist
+con = sqlite3.connect("data/database.db")
+db = con.cursor()
+## Create tables if they don't already exist
+db.execute("CREATE TABLE if not exists Huomenet(id INTEGER PRIMARY KEY, uid INTEGER, hour INTEGER)")
+## Commit changes and close the connection for availability for commands.
+con.commit()
+con.close()
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -50,22 +66,37 @@ async def on_ready():
     await tree.sync(guild=gld)
     print("Commands synced")
 
+## Function for logging Huomenta-calls into the database
+def ratlog(author, time):
+    con = sqlite3.connect("data/database.db")
+    db = con.cursor()
+    db.execute("INSERT INTO Huomenet (uid, hour) VALUES (?, ?)", [author, time])
+    con.commit()
+    con.close()
+
 ## Responds with "Go to work regards Jyrki" if
-## someone wakes up at an unreasonable time
+## someone wakes up at an unreasonable time. Also
+## logs these into a database.
 @client.event
 async def on_message(msg):
     if msg.author == client.user:
         return
     if msg.content.lower() == "huomenta":
+        ratstart = rattimes[0]
+        ratend = rattimes[1]
         aika = datetime.now(pytz.timezone('Europe/Helsinki'))
-        if aika.hour <= 4 or aika.hour >= 11:
+        if aika.hour <= ratend or aika.hour >= ratstart:
             await msg.channel.send("Mene töihin terv. Jyrki.")
+        ratlog(msg.author.id, aika.hour)
 
 ## Test command for checking latency
 ## Also acts as a template for future slash commands
 @tree.command(name = "ping", description = "Ping!", guild=gld)
 async def ping(ctx):
     await ctx.response.send_message(f"Pong! {round(client.latency*1000)} ms")
+
+## Add commands to command tree
+tree.add_command(huomenta.Huomenta(client), guild=gld)
 
 if __name__ == "__main__":
     client.run(token, log_handler=handler)
---
+++ slashcommands/huomenta.py
@@ -0,0 +1,41 @@
+import discord
+from discord.ext import commands
+import json
+import sqlite3
+from discord import app_commands as apc
+
+with open("cfg/cfg.json", "r") as confile:
+    config = json.load(confile)
+owner = config["owner"]
+rattimes = config["rattimes"]
+
+class Huomenta(apc.Group):
+    def __init__(self, client):
+        super().__init__()
+        self.client = client
+
+    @apc.command(name = "huomenta", description = "Huomenta.")
+    async def huomenta(self, ctx):
+        await ctx.response.send_message("Huomenta.")
+
+    @apc.command(name = "stats", description = "Näytä omat tai jonkun muun huomenet.")
+    async def stats(self, ctx, user: discord.Member = None):
+        if user == None:
+            user = ctx.user
+            alku = "Olet"
+        else: alku = f"{user.mention} on"
+        con = sqlite3.connect("data/database.db")
+        db = con.cursor()
+        times = db.execute("SELECT COUNT(*) FROM Huomenet WHERE uid = ?", [user.id]).fetchone()
+        rats = db.execute("SELECT COUNT(*) FROM Huomenet WHERE uid = ? GROUP BY uid HAVING hour >= ? OR hour <= ?", [user.id, rattimes[0], rattimes[1]]).fetchone()
+        if times[0] == 1:
+            kerrat = f"kerran"
+        else:
+            kerrat = f"{times[0]} kertaa"
+        if times == None or times[0] < 1:
+            await ctx.response.send_message("Käyttäjä ei ole ikinä herännyt.")
+        elif rats == None or rats[0] < 1:
+            await ctx.response.send_message(f"{alku} herännyt {kerrat}, ja aina ihmisten aikoihin.")
+        else:
+            await ctx.response.send_message(f"{alku} herännyt {kerrat}, joista {rats[0]} on mennyt rottailuksi.")
+        con.close()
