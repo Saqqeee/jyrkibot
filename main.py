a38fe3470a9d9d51d73fe8c0f47857e97cb3942b
Saqqeee <131715608+Saqqeee@users.noreply.github.com>
2023-04-26 17:31:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,19 +6,31 @@ import pytz
 from datetime import datetime
 handler = logging.FileHandler(filename='loki.log', encoding='utf-8', mode='w')
 
+## Check if the configuration file exists
 if os.path.isfile("cfg/cfg.json"):
+    ## If it does exist, do nothing
     pass
 else:
+    ## If the file doesn't exist, check if the directory exists
+    ## If not, make the directory
     if not os.path.exists("cfg"):
         os.makedirs("cfg")
+    ## Ask the user for some configuration variables and
+    ## dump them into a new config file:
+    ## - TOKEN has to be set for the bot to be used
+    ## - guild can be set if the commands should only be synced
+    ##    to one server; leave None to sync them globally
     data = {
-        'TOKEN': input("Your Discord app's auth token: ")
+        "TOKEN": input("Your Discord app's auth token: "),
+        "guild": None
     }
     with open("cfg/cfg.json", "w+") as confile:
         json.dump(data, confile)
+## Read configurations from file and save them into variables
 with open("cfg/cfg.json", "r") as confile:
     config = json.load(confile)
 token = config["TOKEN"]
+gld = config["guild"]
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -26,12 +38,13 @@ intents.members = True
 
 client = discord.Client(intents=intents)
 
+## Sync command tree
 tree = discord.app_commands.CommandTree(client)
 
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
-    await tree.sync()
+    await tree.sync(guild=gld)
     print("Commands synced")
 
 ## Responds with "Go to work regards Jyrki" if
