d59caa198a5dcfbd027c992ed81aa7b34f16a24d
Saqqeee <131715608+Saqqeee@users.noreply.github.com>
2023-04-29 01:03:06 +0300

    Added Huomenta things

--- main.py
+++ main.py
@@ -5,53 +5,83 @@ import os
 import logging
 import pytz
 import subprocess
+import random
 from slashcommands import huomenta
-from datetime import datetime
+from datetime import datetime, timedelta
 handler = logging.FileHandler(filename='loki.log', encoding='utf-8', mode='w')
 
-## Check if the configuration file exists
-if os.path.isfile("cfg/cfg.json"):
-    ## If it does exist, do nothing
-    pass
-else:
-    ## If the file doesn't exist, check if the directory exists
-    ## If not, make the directory
+# Check if the configuration file exists
+if not os.path.isfile("cfg/cfg.json"):
+    # If the file doesn't exist, check if the directory exists
+    # If not, make the directory
     if not os.path.exists("cfg"):
         os.makedirs("cfg")
-    ## Ask the user for some configuration variables and
-    ## dump them into a new config file:
-    ## - TOKEN has to be set for the bot to be used
-    ## - guild can be set if the commands should only be synced
-    ##    to one server; leave None to sync them globally
+    # Ask the user for some configuration variables and
+    # dump them into a new config file:
+    # - TOKEN has to be set for the bot to be used
+    # - guild can be set if the commands should only be synced
+    #    to one server; leave None to sync them globally
+    # - owner has to be set for certain commands (such as /update) to work
+    # - rattimes is for determining between which hours is it appropriate to wake up
     data = {
         "TOKEN": input("Your Discord app's auth token: "),
         "guild": None,
-        "owner": int(input("Your own Discord ID: ")),
-        "rattimes": [4, 11]
+        "owner": int(input("Your own Discord ID: "))
     }
     with open("cfg/cfg.json", "w+") as confile:
         json.dump(data, confile)
 
-## Read configurations from file and save them into variables
+# Read configurations from file and save them into variables
 with open("cfg/cfg.json", "r") as confile:
     config = json.load(confile)
+
+# Check again if any rows are missing and insert defaults if so
+defaults = {
+        "rattimes": [4, 11],
+        "huomentacooldown": 12,
+        "ultrararechance": 1000,
+        "rarechance": 100
+    }
+changes = False
+for key, value in defaults.items():
+    if key not in config:
+        config.update({key: value})
+        changes = True
+if changes:
+    with open("cfg/cfg.json", "w+") as confile:
+        json.dump(config, confile)
+
 token = config["TOKEN"]
 if config["guild"] == None:
     gld = None
 else:
     gld = discord.Object(id=config["guild"])
 owner = config["owner"]
-rattimes = config["rattimes"]
+ratstart = config["rattimes"][0]
+ratend = config["rattimes"][1]
+huomentacooldown = config["huomentacooldown"]
+ultrararechance = config["ultrararechance"]
+rarechance = config["rarechance"]
 
-## Connect into database or create one if it doesn't already exist
+# Load defaults for populating an empty HuomentaResponses table
+with open("slashcommands/huomenta.json", "r", encoding="utf-8") as hfile:
+    huomentalist = json.load(hfile)
+
+# Connect into database or create one if it doesn't already exist
 if not os.path.exists("data"):
     os.makedirs("data")
 con = sqlite3.connect("data/database.db")
 db = con.cursor()
-## Create tables if they don't already exist
+# Create tables if they don't already exist
 db.execute("CREATE TABLE if not exists Users(id INTEGER PRIMARY KEY, timezone TEXT)")
-db.execute("CREATE TABLE if not exists Huomenet(id INTEGER PRIMARY KEY, uid INTEGER, hour INT)")
-## Commit changes and close the connection for availability for commands.
+db.execute("CREATE TABLE if not exists Huomenet(id INTEGER PRIMARY KEY, uid INTEGER, hour INTEGER)")
+db.execute("CREATE TABLE if not exists HuomentaResponses(id INTEGER PRIMARY KEY, response TEXT UNIQUE, rarity INTEGER, rat INTEGER)")
+db.execute("CREATE TABLE if not exists HuomentaUserStats(id INTEGER PRIMARY KEY, foundlist TEXT, rarelist TEXT, ultralist TEXT, lastdate TEXT)")
+# If table HuomentaResponses is empty, populate it
+responseamount = db.execute("SELECT COUNT(*) FROM HuomentaResponses").fetchone()[0]
+if responseamount == 0:
+    huomenta.populatehuomenta(db, huomentalist)
+# Commit changes and close the connection for availability for commands.
 con.commit()
 con.close()
 
@@ -61,7 +91,7 @@ intents.members = True
 
 client = discord.Client(intents=intents)
 
-## Sync command tree
+# Sync command tree
 tree = discord.app_commands.CommandTree(client)
 
 @client.event
@@ -70,34 +100,93 @@ async def on_ready():
     await tree.sync(guild=gld)
     print("Commands synced")
 
-## Responds with "Go to work regards Jyrki" if
-## someone wakes up at an unreasonable time. Also
-## logs these into a database.
+# Responds with something from the Huomenta list if
+# someone wakes up at an unreasonable time. Also
+# logs these into a database.
 @client.event
 async def on_message(msg):
     if msg.author == client.user:
         return
     if msg.content.lower() == "huomenta":
-        ratstart = rattimes[0]
-        ratend = rattimes[1]
+        # Connect to database and create new user if id is not found, also setting a default time zone
         con = sqlite3.connect("data/database.db")
         db = con.cursor()
+
+        # Insert user id values into tables if they don't exist
         db.execute("INSERT OR IGNORE INTO Users (id, timezone) VALUES (?, ?)", [msg.author.id, "Europe/Helsinki"])
-        tz = db.execute("SELECT timezone FROM Users WHERE id=?", [msg.author.id]).fetchone()
-        aika = datetime.now(pytz.timezone(tz[0]))
-        if aika.hour <= ratend or aika.hour >= ratstart:
-            await msg.channel.send("Mene töihin terv. Jyrki.")
+        # If user has an entry in HuomentaUserStats and their cooldown has not yet passed, stop execution
+        lastdate = db.execute("SELECT lastdate FROM HuomentaUserStats WHERE id=?", [msg.author.id]).fetchone()
+        if lastdate != None and datetime.fromisoformat(lastdate[0]) > datetime.now() - timedelta(hours=huomentacooldown):
+            con.commit()
+            con.close()
+            return
+        # Otherwise get the current hour in user's timezone as well as datetime in UTC format
+        tz = db.execute("SELECT timezone FROM Users WHERE id=?", [msg.author.id]).fetchone()[0]
+        hour = datetime.now(pytz.timezone(tz)).hour
+        aika = datetime.now()
+        
+        # Check for ultra rares and regular rares
+        if random.randint(1,ultrararechance) == 1:
+            rarity = 2
+            rat = 0
+            listtoget = "ultralist"
+        elif random.randint(1,rarechance) == 1:
+            rarity = 1
+            listtoget = "rarelist"
+        else:
+            rarity = 0
+            listtoget = "foundlist"
+        # Rat check. Ultra rares override this
+        if (hour <= ratend or hour >= ratstart) and rarity != 2:
+            rat = 1
+        else:
+            rat = 0
+
+        # Check list of current responses for user
+        userresponses = db.execute("SELECT ? FROM HuomentaUserStats WHERE id=?", [listtoget, msg.author.id]).fetchone()
+        if userresponses != None:
+            foundlist = list(json.loads(userresponses[0]))
+        else:
+            foundlist = []
+        
+        # Gather a list of available responses by rarity and ratness and choose one of them randomly,
+        # saving the response and its id in separate variables
+        responses = db.execute("SELECT id, response FROM HuomentaResponses WHERE rarity=? and rat=?", [rarity, rat]).fetchall()
+        response = random.choice(responses)
+        respid = response[0]
+        respmsg = response[1]
+        # If response is not yet found by user, add it to the list
+        if respid not in foundlist:
+            foundlist.append(respid)
+            foundlist.sort()
+        foundlist = json.dumps(foundlist)
+
+        # Decorate the message if a rare is found
+        if rarity == 1:
+            rarenotif = ":star:"
+        elif rarity == 2:
+            rarenotif = ":star:"*3
+        else:
+            rarenotif = ""
+        await msg.channel.send(rarenotif + respmsg + rarenotif)
         db.execute("INSERT INTO Huomenet (uid, hour) VALUES (?, ?)", [msg.author.id, aika.hour])
+        db.execute("INSERT OR IGNORE INTO HuomentaUserStats(id) VALUES (?)", [msg.author.id])
+        if rarity == 0:
+            db.execute("UPDATE HuomentaUserStats SET foundlist=?, lastdate=? WHERE id=?", [foundlist, aika, msg.author.id])
+        elif rarity == 1:
+            db.execute("UPDATE HuomentaUserStats SET rarelist=?, lastdate=? WHERE id=?", [foundlist, aika, msg.author.id])
+        elif rarity == 2:
+            db.execute("UPDATE HuomentaUserStats SET ultralist=?, lastdate=? WHERE id=?", [foundlist, aika, msg.author.id])
         con.commit()
         con.close()
 
-## Test command for checking latency
-## Also acts as a template for future slash commands
+# Test command for checking latency
+# Also acts as a template for future slash commands
 @tree.command(name = "ping", description = "Pelaa pöytätennistä Jyrkin kanssa!", guild=gld)
 async def ping(ctx):
-    await ctx.response.send_message(f"Pong! {round(client.latency*1000)} ms")
+    await ctx.response.send_message(f"Pong! {round(client.latency*1000)} ms", ephemeral=True)
 
-## This allows an user to set their preferred time zone
+# This allows an user to set their preferred time zone
 @tree.command(name = "timezone", description = "Muuta toiselle aikavyöhykkeelle", guild=gld)
 @discord.app_commands.choices(timezones=[
     discord.app_commands.Choice(name="Helsinki", value="Europe/Helsinki"),
@@ -111,21 +200,23 @@ async def timezone(ctx, timezones: discord.app_commands.Choice[str]):
     db = con.cursor()
     db.execute("INSERT OR IGNORE INTO Users (id, timezone) VALUES (?, ?)", [ctx.user.id, timezones.value])
     db.execute("UPDATE Users SET timezone = ? WHERE id = ?", [timezones.value, ctx.user.id])
-    await ctx.response.send_message(f"{ctx.user.mention}: Aikavyöhykkeeksi vaihdettu {timezones.name}")
+    await ctx.response.send_message(f"{ctx.user.mention}: Aikavyöhykkeeksi vaihdettu {timezones.name}", ephemeral=True)
     con.commit()
     con.close()
 
-@tree.command(name = "update", guild=gld)
+# If this command is called by the owner set in cfg.json,
+# run a script that syncs the repo with origin and restarts the bot
+@tree.command(name = "update", description = "Owner only command", guild=gld)
 async def update(ctx):
     if ctx.user.id == owner:
-        await ctx.response.send_message("Jyrki ottaa päikkärit")
+        await ctx.response.send_message("Jyrki ottaa päikkärit", ephemeral=True)
         await client.close()
         subprocess.call("./update.sh")
         os._exit(1)
     else:
-        await ctx.response.send_message("Jyrki ei nyt tottele sinua")
+        await ctx.response.send_message("Et voi tehdä noin!", ephemeral=True)
 
-## Add commands to command tree
+# Add commands to command tree
 tree.add_command(huomenta.Huomenta(client), guild=gld)
 
 if __name__ == "__main__":
--- slashcommands/huomenta.py
+++ slashcommands/huomenta.py
@@ -8,33 +8,102 @@ with open("cfg/cfg.json", "r") as confile:
 owner = config["owner"]
 rattimes = config["rattimes"]
 
+# This is used by the main program for populating the HuomentaResponses table
+def populatehuomenta(db: sqlite3.Cursor, huomentalist: dict):
+    data = []
+    for x in huomentalist["normalgood"]:
+        data.append((x, 0, 0))
+    for x in huomentalist["normalbad"]:
+        data.append((x, 0, 1))
+    for x in huomentalist["raregood"]:
+        data.append((x, 1, 0))
+    for x in huomentalist["rarebad"]:
+        data.append((x, 1, 1))
+    for x in huomentalist["ultrarare"]:
+        data.append((x, 2, 0))
+    db.executemany("INSERT INTO HuomentaResponses(response, rarity, rat) VALUES (?, ?, ?)", data)
+
 class Huomenta(apc.Group):
     def __init__(self, client):
         super().__init__()
         self.client = client
 
-    @apc.command(name = "huomenta", description = "Huomenta.")
-    async def huomenta(self, ctx):
-        await ctx.response.send_message("Huomenta.")
-
     @apc.command(name = "stats", description = "Näytä omat tai jonkun muun huomenet.")
     async def stats(self, ctx, user: discord.Member = None):
         if user == None:
             user = ctx.user
-            alku = "Olet"
-        else: alku = f"{user.mention} on"
+        embed = discord.Embed(
+            title=f"Huomenta-tilastot käyttäjälle {user.display_name}:",
+            color=discord.Color.dark_magenta()
+        )
+
+        # Fetch information from database for formatting the response
         con = sqlite3.connect("data/database.db")
         db = con.cursor()
-        times = db.execute("SELECT COUNT(*) FROM Huomenet WHERE uid = ?", [user.id]).fetchone()
-        rats = db.execute("SELECT COUNT(*) FROM Huomenet WHERE uid = ? GROUP BY uid HAVING hour >= ? OR hour <= ?", [user.id, rattimes[0], rattimes[1]]).fetchone()
-        if times[0] == 1:
+        times = db.execute("SELECT COUNT(*) FROM Huomenet WHERE uid = ?", [user.id]).fetchone()[0]
+        rats = db.execute("SELECT COUNT(*) FROM Huomenet WHERE uid = ? GROUP BY uid HAVING hour >= ? OR hour <= ?", [user.id, rattimes[0], rattimes[1]]).fetchone()[0]
+        userresponses = db.execute("SELECT foundlist, rarelist, ultralist FROM HuomentaUserStats WHERE id=?", [user.id]).fetchone()
+        morosfound = len(list(json.loads(userresponses[0]))) if userresponses[0] is not None else 0
+        raresfound = len(list(json.loads(userresponses[1]))) if userresponses[1] is not None else 0
+        ultrasfound = len(list(json.loads(userresponses[2]))) if userresponses[2] is not None else 0
+        morototal = db.execute("SELECT COUNT(*) FROM HuomentaResponses WHERE rarity=0").fetchone()[0]
+        raretotal = db.execute("SELECT COUNT(*) FROM HuomentaResponses WHERE rarity=1").fetchone()[0]
+        ultratotal = db.execute("SELECT COUNT(*) FROM HuomentaResponses WHERE rarity=2").fetchone()[0]
+
+        huomentastats = f"Tavallisia huomenia {morosfound}/{morototal}\nHarvinaisia huomenia {raresfound}/{raretotal}\nULTRA-harvinaisia huomenia {ultrasfound}/{ultratotal}"
+
+        if times == 1:
             kerrat = f"kerran"
         else:
-            kerrat = f"{times[0]} kertaa"
-        if times == None or times[0] < 1:
-            await ctx.response.send_message(f"{user.mention} ei ole ikinä herännyt.")
-        elif rats == None or rats[0] < 1:
-            await ctx.response.send_message(f"{alku} herännyt {kerrat}, ja aina ihmisten aikoihin.")
+            kerrat = f"{times} kertaa"
+        if times == None or times < 1:
+            embed.add_field(name="Herätykset", value=f"{user.name} ei ole koskaan herännyt.", inline=False)
+        elif rats == None or rats < 1:
+            embed.add_field(name="Herätykset", value=f"Herätty {kerrat} ja aina ihmisten aikoihin!", inline=False)
         else:
-            await ctx.response.send_message(f"{alku} herännyt {kerrat}, joista {rats[0]} on mennyt rottailuksi.")
+            embed.add_field(name="Herätykset", value=f"Herätty {kerrat}, joista {rats} täysin rottamaiseen aikaan!", inline=False)
+        embed.add_field(name="Jyrkin vastaukset", value=huomentastats, inline=False)
+        await ctx.response.send_message(embed=embed)
         con.close()
+    
+    @apc.command(name = "leaderboard", description = "Kuka on herännyt eniten!")
+    async def leaderboard(self, ctx):
+        con = sqlite3.connect("data/database.db")
+        db = con.cursor()
+        leaders = db.execute("SELECT uid, COUNT(hour) FROM Huomenet GROUP BY uid ORDER BY COUNT(hour) DESC LIMIT 5").fetchall()
+        if len(leaders) == 0:
+            await ctx.response.send_message("Kukaan täällä ei ole herännyt.")
+            return
+        embed = discord.Embed(
+            title="Top 5 herääjät:",
+            color=discord.Color.dark_magenta()
+        )
+        i = 0
+        for leader in leaders:
+            i += 1
+            member = discord.utils.get(ctx.guild.members, id=leader[0])
+            embed.add_field(name=f"**{i}.** {member.display_name}", value=f"Herätyksiä {leader[1]}", inline=False)
+        await ctx.response.send_message(embed=embed)
+
+    
+    @apc.command(name = "add", description = "Owner only command")
+    @discord.app_commands.choices(rarity=[
+        discord.app_commands.Choice(name="Normal", value=0),
+        discord.app_commands.Choice(name="Rare", value=1),
+        discord.app_commands.Choice(name="Ultra rare", value=2)
+    ], ratness=[
+        discord.app_commands.Choice(name="Lammas", value=0),
+        discord.app_commands.Choice(name="Rotta", value=1)
+    ])
+    async def addresponse(self, ctx, response: str, rarity: discord.app_commands.Choice[int], ratness: discord.app_commands.Choice[int]):
+        if ctx.user.id == owner:
+            con = sqlite3.connect("data/database.db")
+            db = con.cursor()
+            if rarity.value == 2:
+                ratness.value = 0
+            db.execute("INSERT INTO HuomentaResponses(response, rarity, rat) VALUES (?, ?, ?)", [response, rarity.value, ratness.value])
+            con.commit()
+            con.close()
+            await ctx.response.send_message("Vastaus lisätty!", ephemeral=True)
+        else:
+            await ctx.response.send_message("Et voi tehdä noin!", ephemeral=True)
