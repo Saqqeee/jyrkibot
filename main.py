c096a8fa8d0995e7451e6cf996b54daa8962cb69
Saqqeee <131715608+Saqqeee@users.noreply.github.com>
2023-05-05 22:18:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -210,9 +210,11 @@ async def timezone(ctx, timezones: discord.app_commands.Choice[str]):
 @tree.command(name = "c7ck", guild=gld)
 async def cock(ctx, user: discord.Member = None):
     if user == None:
+        random.seed(ctx.user.id)
         await ctx.response.send_message(f"Munasi on {random.randint(1,30)} cm pitkä.")
     else:
-        await ctx.response.send_message(f"Käyttäjän {user.display_name} muna on {random.randint(1,30) if user.id != owner else 30} cm pitkä.")
+        random.seed(user.id)
+        await ctx.response.send_message(f"Käyttäjän {user.display_name} muna on {random.randint(1,30)} cm pitkä.")
 
 # If this command is called by the owner set in cfg.json,
 # run a script that syncs the repo with origin and restarts the bot
