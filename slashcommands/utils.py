fe03f37d50a51cb8676399d75b3b57a81079b895
Saqqeee <131715608+Saqqeee@users.noreply.github.com>
2023-05-06 19:11:20 +0300

    Update utils.py

--- slashcommands/utils.py
+++ slashcommands/utils.py
@@ -39,7 +39,7 @@ async def timezone(ctx, timezones: discord.app_commands.Choice[str]):
 @apc.command(name = "c7ck")
 async def cock(ctx, user: discord.Member = None):
     random.seed(ctx.user.id if not user else user.id)
-    cocklength = f"{abs(round(random.gauss(15,10),2))}"
+    cocklength = f"{round(random.gauss(15,10),2)}"
     if user == None or user == ctx.user:
         await ctx.response.send_message(f"Munasi on {cocklength} cm pitkä.")
     else:
