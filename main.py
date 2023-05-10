e331ad1f474a5f7785eea7bf9f4d711172fb6f53
Saqqeee <131715608+Saqqeee@users.noreply.github.com>
2023-05-10 19:50:48 +0300

    Add responses/messages.py
    Moved most of on_message's functionality to its own file

--- main.py
+++ main.py
@@ -3,11 +3,8 @@ import sqlite3
 import json
 import os
 import logging
-import pytz
 import subprocess
-import random
 import sys
-from datetime import datetime, timedelta
 
 handler = logging.FileHandler(filename="loki.log", encoding="utf-8", mode="w")
 
@@ -69,6 +66,7 @@ rarechance = config["rarechance"]
 basicincome = config["basicincome"]
 
 from slashcommands import huomenta, utils, lottery, drunk, alarms
+from responses import messages
 
 # Load defaults for populating an empty HuomentaResponses table
 with open("slashcommands/huomenta.json", "r", encoding="utf-8") as hfile:
@@ -140,133 +138,18 @@ async def on_ready():
     print("Tasks started")
 
 
-# Responds with something from the Huomenta list if
-# someone wakes up at an unreasonable time. Also
-# logs these into a database.
 @client.event
 async def on_message(msg: discord.Message):
     if msg.author == client.user or msg.author.bot:
         return
     if msg.content.lower() == "huomenta":
-        # Connect to database and create new user if id is not found, also setting a default time zone
-        con = sqlite3.connect("data/database.db")
-        db = con.cursor()
-
-        # Insert user id values into tables if they don't exist
-        db.execute(
-            "INSERT OR IGNORE INTO Users (id, timezone) VALUES (?, ?)",
-            [msg.author.id, "Europe/Helsinki"],
-        )
-        # If user has an entry in HuomentaUserStats and their cooldown has not yet passed, stop execution
-        lastdate = db.execute(
-            "SELECT lastdate FROM HuomentaUserStats WHERE id=?", [msg.author.id]
-        ).fetchone()
-        if lastdate != None and datetime.fromisoformat(
-            lastdate[0]
-        ) > datetime.now() - timedelta(hours=huomentacooldown):
-            con.commit()
-            con.close()
-            return
-        # Otherwise get the current hour in user's timezone as well as datetime in UTC format
-        tz = db.execute(
-            "SELECT timezone FROM Users WHERE id=?", [msg.author.id]
-        ).fetchone()[0]
-        hour = datetime.now(pytz.timezone(tz)).hour
-        aika = datetime.now()
-
-        # Check for ultra rares and regular rares
-        if random.randint(1, ultrararechance) == 1:
-            rarity = 2
-            rarenotif = ":star:" * 3
-            rat = 0
-            earn = 50 * basicincome
-        elif random.randint(1, rarechance) == 1:
-            rarity = 1
-            rarenotif = ":star:"
-            earn = 5 * basicincome
-        else:
-            rarity = 0
-            rarenotif = ""
-            earn = basicincome
-        # Rat check. Ultra rares override this
-        if (hour < ratend or hour >= ratstart) and rarity != 2:
-            rat = 1
-        else:
-            rat = 0
-            earn = earn * 2
-
-        # Check list of current responses for user
-        if rarity == 2:
-            userresponses = db.execute(
-                "SELECT ultralist FROM HuomentaUserStats WHERE id=?", [msg.author.id]
-            ).fetchone()
-        elif rarity == 1:
-            userresponses = db.execute(
-                "SELECT rarelist FROM HuomentaUserStats WHERE id=?", [msg.author.id]
-            ).fetchone()
-        else:
-            userresponses = db.execute(
-                "SELECT foundlist FROM HuomentaUserStats WHERE id=?", [msg.author.id]
-            ).fetchone()
-        if userresponses[0] != None:
-            foundlist = list(json.loads(userresponses[0]))
-        else:
-            foundlist = []
-
-        # Gather a list of available responses by rarity and ratness and choose one of them randomly,
-        # saving the response and its id in separate variables
-        responses = db.execute(
-            "SELECT id, response FROM HuomentaResponses WHERE rarity=? and rat=?",
-            [rarity, rat],
-        ).fetchall()
-        response = random.choice(responses)
-        respid = response[0]
-        respmsg = response[1]
-        # If response is not yet found by user, add it to the list
-        if respid not in foundlist:
-            foundlist.append(respid)
-            foundlist.sort()
-        foundlist = json.dumps(foundlist)
-
-        # Send response and save stuff into databases
-        db.execute(
-            "INSERT INTO Huomenet (uid, hour) VALUES (?, ?)", [msg.author.id, hour]
-        )
-        db.execute(
-            "INSERT OR IGNORE INTO HuomentaUserStats(id) VALUES (?)", [msg.author.id]
-        )
-        if rarity == 0:
-            db.execute(
-                "UPDATE HuomentaUserStats SET foundlist=?, lastdate=? WHERE id=?",
-                [foundlist, aika, msg.author.id],
-            )
-        elif rarity == 1:
-            db.execute(
-                "UPDATE HuomentaUserStats SET rarelist=?, lastdate=? WHERE id=?",
-                [foundlist, aika, msg.author.id],
-            )
-        elif rarity == 2:
-            db.execute(
-                "UPDATE HuomentaUserStats SET ultralist=?, lastdate=? WHERE id=?",
-                [foundlist, aika, msg.author.id],
-            )
-        db.execute(
-            "INSERT OR IGNORE INTO LotteryPlayers(id, credits) VALUES (?, 0)",
-            [msg.author.id],
-        )
-        db.execute(
-            "UPDATE LotteryPlayers SET credits=credits+? WHERE id=?",
-            [earn, msg.author.id],
-        )
-        con.commit()
-        con.close()
-        await msg.channel.send(rarenotif + respmsg + rarenotif)
+        await messages.goodmorning(msg)
 
 
 # If this command is called by the owner set in cfg.json,
 # run a script that syncs the repo with origin and restarts the bot
 @tree.command(name="update", description="Owner only command", guild=gld)
-async def update(ctx):
+async def update(ctx: discord.Interaction):
     if ctx.user.id == owner:
         await ctx.response.send_message("Jyrki ottaa päikkärit", ephemeral=True)
         subprocess.Popen("./update.sh")
@@ -279,7 +162,7 @@ async def update(ctx):
 # Test command for checking latency
 # Also acts as a template for future slash commands
 @tree.command(name="ping", description="Pelaa pöytätennistä Jyrkin kanssa!", guild=gld)
-async def ping(ctx):
+async def ping(ctx: discord.Interaction):
     await ctx.response.send_message(
         f"Pong! {round(client.latency*1000)} ms", ephemeral=True
     )
---
+++ responses/messages.py
@@ -0,0 +1,136 @@
+import discord
+import sqlite3
+import random
+import json
+import pytz
+from datetime import datetime, timedelta
+
+with open("cfg/cfg.json", "r") as confile:
+    config = json.load(confile)
+ratstart = config["rattimes"][0]
+ratend = config["rattimes"][1]
+huomentacooldown = config["huomentacooldown"]
+ultrararechance = config["ultrararechance"]
+rarechance = config["rarechance"]
+basicincome = config["basicincome"]
+
+
+async def goodmorning(msg: discord.Message):
+    """
+    Send a good morning message and do some database magic
+    """
+
+    # Connect to database and create new user if id is not found, also setting a default time zone
+    con = sqlite3.connect("data/database.db")
+    db = con.cursor()
+
+    # Insert user id values into tables if they don't exist
+    db.execute(
+        "INSERT OR IGNORE INTO Users (id, timezone) VALUES (?, ?)",
+        [msg.author.id, "Europe/Helsinki"],
+    )
+    # If user has an entry in HuomentaUserStats and their cooldown has not yet passed, stop execution
+    lastdate = db.execute(
+        "SELECT lastdate FROM HuomentaUserStats WHERE id=?", [msg.author.id]
+    ).fetchone()
+    if lastdate != None and datetime.fromisoformat(
+        lastdate[0]
+    ) > datetime.now() - timedelta(hours=huomentacooldown):
+        con.commit()
+        con.close()
+        return
+    # Otherwise get the current hour in user's timezone as well as datetime in UTC format
+    tz = db.execute(
+        "SELECT timezone FROM Users WHERE id=?", [msg.author.id]
+    ).fetchone()[0]
+    hour = datetime.now(pytz.timezone(tz)).hour
+    aika = datetime.now()
+
+    # Check for ultra rares and regular rares
+    if random.randint(1, ultrararechance) == 1:
+        rarity = 2
+        rarenotif = ":star:" * 3
+        rat = 0
+        earn = 50 * basicincome
+    elif random.randint(1, rarechance) == 1:
+        rarity = 1
+        rarenotif = ":star:"
+        earn = 5 * basicincome
+    else:
+        rarity = 0
+        rarenotif = ""
+        earn = basicincome
+    # Rat check. Ultra rares override this
+    if (hour < ratend or hour >= ratstart) and rarity != 2:
+        rat = 1
+    else:
+        rat = 0
+        earn = earn * 2
+
+    # Check list of current responses for user
+    if rarity == 2:
+        userresponses = db.execute(
+            "SELECT ultralist FROM HuomentaUserStats WHERE id=?",
+            [msg.author.id],
+        ).fetchone()
+    elif rarity == 1:
+        userresponses = db.execute(
+            "SELECT rarelist FROM HuomentaUserStats WHERE id=?", [msg.author.id]
+        ).fetchone()
+    else:
+        userresponses = db.execute(
+            "SELECT foundlist FROM HuomentaUserStats WHERE id=?",
+            [msg.author.id],
+        ).fetchone()
+    if userresponses[0] != None:
+        foundlist = list(json.loads(userresponses[0]))
+    else:
+        foundlist = []
+
+    # Gather a list of available responses by rarity and ratness and choose one of them randomly,
+    # saving the response and its id in separate variables
+    responses = db.execute(
+        "SELECT id, response FROM HuomentaResponses WHERE rarity=? and rat=?",
+        [rarity, rat],
+    ).fetchall()
+    response = random.choice(responses)
+    respid = response[0]
+    respmsg = response[1]
+    # If response is not yet found by user, add it to the list
+    if respid not in foundlist:
+        foundlist.append(respid)
+        foundlist.sort()
+    foundlist = json.dumps(foundlist)
+
+    # Send response and save stuff into databases
+    db.execute("INSERT INTO Huomenet (uid, hour) VALUES (?, ?)", [msg.author.id, hour])
+    db.execute(
+        "INSERT OR IGNORE INTO HuomentaUserStats(id) VALUES (?)",
+        [msg.author.id],
+    )
+    if rarity == 0:
+        db.execute(
+            "UPDATE HuomentaUserStats SET foundlist=?, lastdate=? WHERE id=?",
+            [foundlist, aika, msg.author.id],
+        )
+    elif rarity == 1:
+        db.execute(
+            "UPDATE HuomentaUserStats SET rarelist=?, lastdate=? WHERE id=?",
+            [foundlist, aika, msg.author.id],
+        )
+    elif rarity == 2:
+        db.execute(
+            "UPDATE HuomentaUserStats SET ultralist=?, lastdate=? WHERE id=?",
+            [foundlist, aika, msg.author.id],
+        )
+    db.execute(
+        "INSERT OR IGNORE INTO LotteryPlayers(id, credits) VALUES (?, 0)",
+        [msg.author.id],
+    )
+    db.execute(
+        "UPDATE LotteryPlayers SET credits=credits+? WHERE id=?",
+        [earn, msg.author.id],
+    )
+    con.commit()
+    con.close()
+    await msg.channel.send(rarenotif + respmsg + rarenotif)
