5cb8546fd32c784c5ad2431e4887a55d5bc1d176
Saqqeee <131715608+Saqqeee@users.noreply.github.com>
2023-06-19 22:33:55 +0300

    feat: add current weather command

--- slashcommands/weather.py
+++ slashcommands/weather.py
@@ -9,14 +9,16 @@ from sqlalchemy.orm import Session
 from sqlalchemy import select
 
 
-async def _getforecast(place: str, parameters: str, time: Literal["24h", "3d"]):
+async def _getweatherdata(
+    querystring: str, parameters: str, place: str, time: Literal["24h", "3d", "now"]
+):
     """
-    Get forecast data from FMI
+    Get weather data from FMI
 
     Parameters
     ----------
     place: `str`
-        The place (city etc.) to get the forecast for
+        The place (city etc.) to get the data for
     parameters: `str`
         A comma-separated list in string form of weather parameters to query for: `"Temperature,WindSpeedMS,WindDirection,PrecipitationAmount"`
     time: `Literal['24h', '3d']`
@@ -30,18 +32,22 @@ async def _getforecast(place: str, parameters: str, time: Literal["24h", "3d"]):
     if time == "24h":
         endtime = starttime + timedelta(hours=24)
         timestep = "180"
-    elif time == "3d":
+    if time == "3d":
         endtime = starttime + timedelta(hours=60)
         timestep = "360"
+    if time == "now":
+        endtime = None
+        starttime -= timedelta(hours=1)
+        timestep = None
 
     query = download_stored_query(
-        "fmi::forecast::harmonie::surface::point::multipointcoverage",
+        querystring,
         args=[
             f"place={place}",
             f"parameters={parameters}",
             f"starttime={starttime}",
-            f"endtime={endtime}",
-            f"timestep={timestep}",
+            f"endtime={endtime}" if endtime else "",
+            f"timestep={timestep}" if timestep else "",
         ],
     )
 
@@ -69,9 +75,21 @@ async def _getwindarrow(angle):
 class Weather(apc.Group):
     def __init__(self):
         super().__init__()
-        self.qparams = "Temperature,WindSpeedMS,WindDirection,PrecipitationAmount"
+        self.forecastparams = (
+            "Temperature,WindSpeedMS,WindDirection,PrecipitationAmount"
+        )
+        self.forecaststring = (
+            "fmi::forecast::harmonie::surface::point::multipointcoverage"
+        )
+        self.currentweatherstring = "fmi::observations::weather::multipointcoverage"
+        self.currentweatherparams = (
+            "t2m,ws_10min,wg_10min,wd_10min,rh,r_1h,ri_10min,snow_aws,n_man,wawa"
+        )
 
-    @apc.command(name="forecast", description="Sääennuste haluamallesi paikkakunnalle")
+    @apc.command(
+        name="forecast",
+        description="Sääennuste haluamallesi paikkakunnalle Pohjoismaiden alueella",
+    )
     @apc.describe(
         place="Haluamasi paikannimi", timespan="Kuinka pitkän ajan ennusteen haluat"
     )
@@ -81,7 +99,9 @@ class Weather(apc.Group):
         place: str,
         timespan: Literal["24h", "3d"] = "24h",
     ):
-        forecastdata = await _getforecast(place, self.qparams, timespan)
+        forecastdata = await _getweatherdata(
+            self.forecaststring, self.forecastparams, place, timespan
+        )
 
         # If forecast data is not found, send an error message and return
         if not forecastdata.data:
@@ -121,5 +141,82 @@ class Weather(apc.Group):
             fieldvalue = f"\U0001F321 {temp} {chr(176)}C | {windarrow} {wind} m/s | \U0001F327 {rain} mm"
 
             embed.add_field(name=timestamp, value=fieldvalue, inline=False)
+
+        await ctx.response.send_message(embed=embed)
+
+    @apc.command(name="current", description="Tämän hetkinen sää Suomen alueella")
+    @apc.describe(place="Paikannimi")
+    async def currentweather(self, ctx: discord.Interaction, place: str):
+        weatherdata = await _getweatherdata(
+            self.currentweatherstring, self.currentweatherparams, place, "now"
+        )
+
+        # If weather data is not found, send an error message and return
+        if not weatherdata.data:
+            await ctx.response.send_message(
+                content=f"Säätä ei löytynyt paikalle {place.title()}",
+                ephemeral=True,
+            )
+            return
+
+        # Get the latest readings
+        timekey: datetime = max(weatherdata.data.keys())
+        timeddata = weatherdata.data[timekey]
+        locationname = list(timeddata.keys())[0]
+        weathervalues = timeddata[locationname]
+
+        # Set variables
+        temp = weathervalues["Air temperature"]["value"]
+        windarrow = await _getwindarrow(weathervalues["Wind direction"]["value"])
+        windspeed = weathervalues["Wind speed"]["value"]
+        windgust = weathervalues["Gust speed"]["value"]
+        rainintensity = weathervalues["Precipitation intensity"]["value"]
+        humidity = weathervalues["Relative humidity"]["value"]
+        snowdepth = weathervalues["Snow depth"]["value"]
+        cloudamount = weathervalues["Cloud amount"]["value"]
+        cloudstrings = {
+            0.0: "Selkeää",
+            1.0: "Melkein selkeää",
+            2.0: "Melkein selkeää",
+            3.0: "Puolipilvistä",
+            4.0: "Puolipilvistä",
+            5.0: "Puolipilvistä",
+            6.0: "Melkein pilvistä",
+            7.0: "Melkein pilvistä",
+            8.0: "Pilvistä",
+        }
+
+        # Initialize response embed
+        embed = discord.Embed(
+            color=discord.Color.dark_magenta(),
+            title=f"Sää kohteessa {locationname}",
+            timestamp=pytz.timezone("Europe/Helsinki").fromutc(timekey),
+        )
+
+        # Add fields to embed one by one
+        embed.add_field(
+            name="\U0001F321 Lämpötila",
+            value=f"{temp} {chr(176)}C",
+            inline=True,
+        )
+        embed.add_field(
+            name="\U0001F4A8 Tuuli",
+            value=f"{windarrow} {windspeed} ({windgust}) m/s",
+            inline=True,
+        )
+        embed.add_field(
+            name="\U0001F327 Sade", value=f"{rainintensity} mm", inline=True
+        )
+        embed.add_field(
+            name="\U0001F4A6 Ilmankosteus", value=f"{humidity} %", inline=True
+        )
+        embed.add_field(
+            name="\U00002601 Pilvisyys",
+            value=f"{cloudstrings[cloudamount]} ({int(cloudamount)}/8)",
+        )
+        if snowdepth > 0:
+            embed.add_field(
+                name="\U00002744 Lumensyvyys", value=f"{snowdepth} cm", inline=True
+            )
 
         await ctx.response.send_message(embed=embed)
