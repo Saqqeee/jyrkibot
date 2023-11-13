75ecae8d3446b8154eb6e2855f409d98107b85f4
Saqqeee <131715608+Saqqeee@users.noreply.github.com>
2023-11-13 11:34:15 +0200

    style: separate view classes from rps class

--- minigames/rps.py
+++ minigames/rps.py
@@ -17,7 +17,7 @@ class Rps:
         await self.find_player()
 
     async def find_player(self):
-        button_view = self.PlayView()
+        button_view = PlayView(self)
 
         response = await self.ctx.channel.send(
             content=f"{self.player1.name} wants to play Rock, Paper, Scissors",
@@ -33,8 +33,8 @@ class Rps:
         await self.game(button_view.ctx)
 
     async def game(self, ctx2: discord.Interaction):
-        p1c = self.SelectView(self.ctx)
-        p2c = self.SelectView(ctx2)
+        p1c = SelectView(self.ctx)
+        p2c = SelectView(ctx2)
 
         msg1 = await self.ctx.followup.send(content="Pick one", view=p1c)
         msg2 = await ctx2.followup.send(content="Pick one", view=p2c, ephemeral=True)
@@ -51,51 +51,62 @@ class Rps:
                 f"{self.ctx.user.mention} {RPS_ICONS[p1c.choice]} - {RPS_ICONS[p2c.choice]} {ctx2.user.mention}"
             )
 
-    class PlayView(discord.ui.View):
+
+class PlayView(discord.ui.View):
+    def __init__(self, parent):
+        super().__init__(timeout=None)
+
+        self.parent = parent
+        self.button = self.PlayButton()
+        self.add_item(self.button)
+        self.ctx: discord.Interaction | None = None
+
+    async def interaction_check(self, ctx: discord.Interaction):
+        if ctx.user == self.parent.player1:
+            await ctx.response.send_message(
+                content="You can't play with yourself", ephemeral=True
+            )
+            return False
+        return True
+
+    class PlayButton(discord.ui.Button):
         def __init__(self):
-            super().__init__(timeout=None)
+            super().__init__()
 
-            self.button = self.PlayButton()
-            self.add_item(self.button)
-            self.ctx: discord.Interaction | None = None
+            self.label = "Play!"
+            self.style = discord.ButtonStyle.blurple
 
-        class PlayButton(discord.ui.Button):
-            def __init__(self):
-                super().__init__()
+        async def callback(self, ctx: discord.Interaction):
+            self.view.ctx = ctx
+            await self.view.ctx.response.defer(ephemeral=True, thinking=False)
+            self.view.stop()
 
-                self.label = "Play!"
-                self.style = discord.ButtonStyle.blurple
 
-            async def callback(self, ctx: discord.Interaction):
-                self.view.ctx = ctx
-                await self.view.ctx.response.defer(ephemeral=True, thinking=False)
-                self.view.stop()
+class SelectView(discord.ui.View):
+    def __init__(self, ctx: discord.Interaction):
+        super().__init__(timeout=None)
+        self.ctx = ctx
 
-    class SelectView(discord.ui.View):
-        def __init__(self, ctx: discord.Interaction):
-            super().__init__(timeout=None)
-            self.ctx = ctx
+        poss = ["rock", "paper", "scissors"]
+        for item in poss:
+            self.add_item(self.ChooseButton(item))
 
-            poss = ["rock", "paper", "scissors"]
-            for item in poss:
-                self.add_item(self.ChooseButton(item))
+        self.choice: str | None = None
 
-            self.choice: str | None = None
+    def on_timeout(self):
+        self.ctx.channel.send(content=f"{self.ctx.user.name} is bald")
 
-        def on_timeout(self):
-            self.ctx.channel.send(content=f"{self.ctx.user.name} is bald")
+    class ChooseButton(discord.ui.Button):
+        def __init__(self, choice: str):
+            super().__init__()
+            self.choice = choice
 
-        class ChooseButton(discord.ui.Button):
-            def __init__(self, choice: str):
-                super().__init__()
-                self.choice = choice
+            self.emoji = discord.PartialEmoji(name=RPS_ICONS[self.choice])
 
-                self.emoji = discord.PartialEmoji(name=RPS_ICONS[self.choice])
-
-            async def callback(self, ctx: discord.Interaction):
-                self.view.choice = self.choice
-                self.view.stop()
-                await ctx.response.send_message(
-                    content=f"You picked {self.choice} {RPS_ICONS[self.choice]}",
-                    ephemeral=True,
-                )
+        async def callback(self, ctx: discord.Interaction):
+            self.view.choice = self.choice
+            self.view.stop()
+            await ctx.response.send_message(
+                content=f"You picked {self.choice} {RPS_ICONS[self.choice]}",
+                ephemeral=True,
+            )
