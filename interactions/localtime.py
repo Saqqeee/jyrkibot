987823a14f0826ac3cd97f9c474ee73675bc999a
Saqqeee <131715608+Saqqeee@users.noreply.github.com>
2024-03-06 23:29:10 +0200

    feat: make local time feature a bit better

--- interactions/localtime.py
+++ interactions/localtime.py
@@ -12,9 +12,11 @@ async def localtime(ctx: discord.Interaction, member: discord.Member):
     with Session(engine) as db:
         tz = db.scalar(select(Users.timezone).where(Users.id == member.id))
 
+    msg: str
     if not tz:
-        tz = "Europe/Helsinki"
+        msg = f"{member.display_name} has not set their time zone"
+    else:
+        time = datetime.now(tz=pytz.timezone(tz)).strftime("%H:%M on %A (UTC %z)")
+        msg = f"Time for {member.display_name} in {tz}: {time}"
 
-    time = datetime.now(tz=pytz.timezone(tz))
-
-    await ctx.response.send_message(content=f"Time in {tz}: {time}")
+    await ctx.response().send_message(content=msg, ephemeral=True)
